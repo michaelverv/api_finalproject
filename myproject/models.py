b75a534553b84054daf466f96155ef7b6fc70106
michaelverv <r0844989@student.thomasmore.be>
2023-12-21 17:09:02 +0100

    first push

---
+++ myproject/models.py
@@ -0,0 +1,65 @@
+# link naar db engine
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, Float
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class Band(Base):
+    __tablename__ = "bands"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    genre = Column(String)
+    lead_singer = Column(String)
+    is_still_active = Column(Boolean, default=True)
+
+    albums = relationship("Album", back_populates="band")
+
+
+class Album(Base):
+    __tablename__ = "albums"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    duration = Column(Float)
+    amount_of_songs = Column(Integer)
+    release_date = Column(Integer)
+
+    band_id = Column(Integer, ForeignKey("bands.id"))
+    band = relationship("Band", back_populates="albums")
+    songs = relationship("Song", back_populates="album")
+
+
+class Song(Base):
+    __tablename__ = "songs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    duration = Column(Float)
+
+    album_id = Column(Integer, ForeignKey("albums.id"))
+    album = relationship("Album", back_populates="songs")
+    playlist = relationship("Playlist", back_populates="songs")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    hashed_password = Column(String)
+    email = Column(String, unique=True, index=True)
+
+    playlists = relationship("Playlist", back_populates="user")
+
+
+class Playlist(Base):
+    __tablename__ = "playlists"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="playlists")
+    song = relationship("Playlist", back_populates="playlist")
---
+++ myproject/schemas.py
@@ -0,0 +1,107 @@
+from pydantic import BaseModel
+
+
+# Song Base
+class SongBase(BaseModel):
+    duration: float
+    name: str
+
+
+# Song OUT
+class Song(SongBase):
+    id: int
+    album_id: int
+
+    class Config:
+        orm_mode = True
+
+
+# Song IN
+class SongCreate(SongBase):
+    pass
+
+
+# Album Base
+class AlbumBase(BaseModel):
+    duration: float
+    amount_of_songs: int
+    release_date: int
+    name: str
+
+
+# Album OUT
+class Album(AlbumBase):
+    band_id: int
+    id: int
+    songs: list[Song] = []
+
+    class Config:
+        orm_mode = True
+
+
+# Album IN
+class AlbumCreate(AlbumBase):
+    pass
+
+
+# Band Base
+class BandBase(BaseModel):
+    name: str
+    is_still_active: bool
+    lead_singer: str
+    genre: str
+
+
+# Band OUT
+class Band(BandBase):
+    id: int
+    albums: list[Album] = []
+
+    class Config:
+        orm_mode = True
+
+
+# Band IN
+class BandCreate(BandBase):
+    pass
+
+
+# Playlist Base
+class PlaylistBase(BaseModel):
+    name: str
+    songs: list[Song] = []
+    description: str
+    user_id: int
+
+
+# Playlist OUT
+class Playlist(PlaylistBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+# Playlist IN
+class PlaylistCreate(PlaylistBase):
+    pass
+
+
+# User Base
+class UserBase(BaseModel):
+    username: str
+    email: str
+    playlists: list[Playlist] = []
+
+
+# User OUT
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+# User IN
+class UserCreate(UserBase):
+    hashed_password: int
